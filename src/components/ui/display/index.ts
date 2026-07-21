d9bf0c519a7293fa46ee4b80ad18f1a656942ce0
tkahmedkamal-exercise <t.ahmed.elgamal22@gmail.com>
2026-07-21 21:26:28 +0300

    feat: Add info card component

--- src/components/ui/display/index.ts
+++ src/components/ui/display/index.ts
@@ -1,3 +1,4 @@
 export { default as Avatar } from "./avatar/avatar";
+export { default as InfoCard } from "./info-card/info-card";
 export { default as Typography } from "./typography";
 export { default as UserGreeting } from "./user-greeting/user-greeting";
